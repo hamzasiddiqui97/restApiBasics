be239064bd70ded3d44540a9928a0c61eef4979a
Hamza <hamzasiddiqui379@gmail.com>
2023-03-25 03:51:44 +0500

    working on fetching hotel_details

--- lib/hotel_details.dart
+++ lib/hotel_details.dart
@@ -1,42 +1,58 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
-import 'services/hotel_details_services.dart';
+import 'package:http/http.dart' as http;
+import 'models/hotel_prices_details.dart';
 
 class HotelScreen extends StatefulWidget {
-  const HotelScreen({super.key});
+  const HotelScreen({Key? key}) : super(key: key);
 
   @override
   State<HotelScreen> createState() => _HotelScreenState();
 }
 
 class _HotelScreenState extends State<HotelScreen> {
-  dynamic hotelData;
+  List<HotelDetailsPriceModel> hotelDetailList = [];
 
   @override
   void initState() {
     super.initState();
-    _fetchHotelData();
+    getHotelDetail();
   }
 
-  Future<void> _fetchHotelData() async {
-    try {
-      final data = await getHotelData();
-      setState(() {
-        hotelData = data;
-      });
-    } catch (e) {
-      print('Error fetching hotel data: $e');
+  Future<void> getHotelDetail() async {
+    final response = await http.get(
+      Uri.parse(
+          'priceline-com.p.rapidapi.com/hotel/8794805?checkin_date=2021-01-22&checkout_date=2021-01-23&rooms=1&currency=PKR'),
+      headers: {
+        'X-RapidAPI-Key': '5843457f22msh1543769225b1348p154628jsn77fe1eb72d25',
+        'X-RapidAPI-Host': 'priceline-com.p.rapidapi.com',
+        'useQueryString': 'true',
+      },
+    );
+
+    var data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        hotelDetailList.add(HotelDetailsPriceModel.fromJson(i));
+      }
+      setState(() {});
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("API Application")),
-      body: Center(
-        child: hotelData == null
-            ? const CircularProgressIndicator()
-            : Text(hotelData.toString()),
+      body: ListView.builder(
+        itemCount: hotelDetailList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(hotelDetailList[index].name ?? ''),
+            subtitle: Text(hotelDetailList[index].description ?? ''),
+            leading: Image.network(hotelDetailList[index].thumbnailUrl ?? ''),
+          );
+        },
       ),
     );
   }
